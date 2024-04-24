63e80facac2576987fe83705e35ee5859967c834
srividhya859 <srividhya859@gmail.com>
2024-04-24 17:16:23 +0530

    portfolio

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { EducationComponent } from './pages/education/education.component';
+import { SkillsComponent } from './pages/skills/skills.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+          {path:'home',redirectTo:'home',pathMatch:'full'},
+          {path:'',component:HomeComponent},
+          {path: 'about', component:AboutComponent},
+          {path:'contact', component:ContactComponent},
+          {path:'education', component:EducationComponent},
+          {path:'skills', component:SkillsComponent}
+];
