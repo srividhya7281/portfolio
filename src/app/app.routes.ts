4e4d20f77f9e656177d6c0c95f90bebc4e5ed6ba
srividhya859 <srividhya859@gmail.com>
2024-04-26 17:08:32 +0530

    portfolio

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,15 @@ import { AboutComponent } from './pages/about/about.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { EducationComponent } from './pages/education/education.component';
 import { SkillsComponent } from './pages/skills/skills.component';
+import { VideosComponent } from './pages/videos/videos.component';
 
 export const routes: Routes = [
           {path:'home',redirectTo:'home',pathMatch:'full'},
-          {path:'',component:HomeComponent},
+          {path:'home',component:HomeComponent},
           {path: 'about', component:AboutComponent},
           {path:'contact', component:ContactComponent},
           {path:'education', component:EducationComponent},
-          {path:'skills', component:SkillsComponent}
+          {path:'skills', component:SkillsComponent},
+          {path:'education', component:EducationComponent},
+          {path:'videos', component:VideosComponent}
 ];
